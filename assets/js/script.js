d8101d6bfed279a29f85509f0552645152b9e27e
Sebi <isadorasebastiana@gmail.com>
2024-07-30 15:56:24 -0400

    Añadiendo alerts contacto, responsividad básica, terminando:(

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    $('button').click(function() {
+        let email = $('#text22');
+        let message = $('#text24');
+        let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-
-    
-    
+        if (email !== "" && message !== "") {
+            if (emailPattern.test(email)) {
+                alert("Mensaje enviado con éxito!");
+            } else {
+                alert("Por favor, ingrese un correo electrónico válido.");
+            }
+        } else {
+            alert("Por favor, complete ambos campos.");
+        }
+    });
 });
